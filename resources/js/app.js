ed2fb935159166b409f201f0f5d3302d3fa3b60b
codebytesfl <codebytesfl@gmail.com>
2021-07-20 14:50:23 -0400

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import vuetify from './vuetify';
+
+createInertiaApp({
+  resolve: name => require(`./pages/${name}`),
+  setup({ el, app, props }) {
+    new Vue({
+      render: h => h(app, props),
+      vuetify
+    }).$mount(el)
+  },
+})
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    $restaurants = [
+        [
+            'id' => 1,
+            'name' => 'Cafe Badilico',
+            'type' => 'Italian, Cafe',
+            'description' => 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.',
+            'img' => 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+            'ratings' => [
+                'score' => 4.5,
+                'total' => 403
+            ]
+        ],
+        [
+            'id' => 2,
+            'name' => 'Grilled Cheese Master',
+            'type' => 'American, Fast',
+            'description' => 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.',
+            'img' => 'https://images.unsplash.com/photo-1528736235302-52922df5c122?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JpbGxlZCUyMGNoZWVzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+            'ratings' => [
+                'score' => 4.5,
+                'total' => 403
+            ]
+        ],
+        [
+            'id' => 3,
+            'name' => 'Asian Bistro',
+            'type' => 'Asian, Casual',
+            'description' => 'Small plates - an intimate setting with 12 indoor seats plus patio seating.',
+            'img' => 'https://images.unsplash.com/photo-1569246294372-ed319c674f14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
+            'ratings' => [
+                'score' => 4.5,
+                'total' => 403
+            ]
+        ]
+    ];
+    
+    return Inertia::render('home', ['restaurants' => $restaurants]);
+});
+
+Route::get('/contact', function() {
+    return Inertia::render('contact');
 });
