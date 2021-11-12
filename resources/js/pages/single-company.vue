a186e7a770f57f26e9e511c141710e65c17f7f39
Braunjack03 <Braunjack03@gmail.com>
2021-11-12 11:09:32 +0530

    single view, company view

--- resources/js/pages/single-company.vue
+++ resources/js/pages/single-company.vue
@@ -1,69 +1,72 @@
 <template>
 <Layout>
-    <v-app app>
-        <v-container>
-            <v-card class="mx-auto px-12 py-8" elevation="2">
+    <Head title="Create Job" />
+    <section class="relative" data-app>
+          <div class="max-w-6xl mx-auto px-4 sm:px-6">
+            <div class="pt-32 pb-12 md:pt-40 md:pb-20">
+
               <div v-if="$page.props.flash.message" class="mt-2 success">
             {{ $page.props.flash.message }}
           </div> 
             <v-row >
-              <v-col cols="3"> 
+              <v-col lg="3" md="4" sm="5" cols="12" > 
              <v-img
                       :src="data.logo_image_url"
                       max-height="150"
                       max-width="250"
                     ></v-img>
             </v-col>        
-            <v-col class="mt-12" md="6" cols="8">        
-            <h2>{{data.name}} <v-btn v-if="(data.unclaimed) == 1 && (data.role)==3" v-on:click="claimProfile(data.uuid)" class="claim-profile">(Claim this Profile)</v-btn> </h2>
-            <p>{{data.industry_types}}</p>
-               </v-col>
-            </v-row>
-            <v-row >
-              <v-col offset-md="3" cols="3" md="2"> 
-              Location : <br/><strong>{{data.location}}</strong>
-                </v-col>        
-              <v-col cols="3"> 
-              Local Employees: <strong>{{data.local_employees}}</strong><br/>
-              Global Employees: <strong>{{data.global_employees}}</strong>
-              </v-col>        
-              <v-col cols="3"> 
-              Year Founded: <strong>{{data.year_founded}}</strong><br/>
+            <v-col lg="9" md="8" sm="7" cols="12" >        
+              <h2 class="post-title mb-0">{{data.name}} <v-btn v-if="(data.unclaimed) == 1 && (data.role)==3" v-on:click="claimProfile(data.uuid)" class="claim-profile ml-auto mr-0">(Claim this Profile)</v-btn> 
+                <v-btn v-on:click="claimProfile(data.uuid)" class="link-text ml-2" text>(Claim this Profile)</v-btn>
+              </h2>
+              <p class="types-title mt-0">{{data.industry_types}}</p>
+                <v-row class="mt-0">
+                  <v-col cols="12" sm="6" md="4">
+                    <div class="extra-info"> 
+                        Location : <span><strong>{{data.location}}</strong></span>
+                    </div>
+                    </v-col>        
+                  <v-col cols="12" sm="6" md="4"> 
+                    <div class="extra-info">
+                      Local Employees: <strong>{{data.local_employees}}</strong><br/>
+                      Global Employees: <strong>{{data.global_employees}}</strong>
+                    </div>
+                  </v-col>        
+                  <v-col cols="12" sm="6" md="4"> 
+                    <div class="extra-info">
+                      Year Founded: <strong>{{data.year_founded}}</strong><br/>
+                    </div>
 
-              <v-btn icon :href="data.website_url" target="_blank" text>
-                View website
-              </v-btn>
-              </v-col>  
+                  <v-btn :href="data.website_url" target="_blank" class="bg-purple-600 hover:bg-purple-700 mt-2">
+                    View website
+                  </v-btn>
+                  </v-col>  
+                </v-row>
+              </v-col> 
             </v-row>
 
             <v-row >
-              <v-col cols="8" md="8"> 
-                <h3>About {{data.name}}</h3>
-                <div v-html="data.mission"></div>
+              <v-col cols="12" md="12"> 
+                <h3 class="post-title">About {{data.name}}</h3>
+                <div class="desc" v-html="data.mission"></div>
               </v-col>  
             </v-row>
              <v-row >
               <v-col cols="12" class="pa-2"> 
-                <h3>Jobs at {{data.name}}</h3>
+                <h3 class="section-title">Jobs at {{data.name}}</h3>
                   <ul v-if="job_posts" id="example-1">
                     <li v-for="company in job_posts" :key="company.id">
-                      <v-row no-gutters class="mt-5">
-                        <v-col cols="8" sm="6" md="8" >
-
-                        
-
-                              <h4><a :href="`/jobs/${data.slug}/${company.slug}`">{{ company.name }} ({{ company.location_id }})</a></h4>
-                              <div v-html="company.content"></div>
-                        </v-col>
-                      </v-row>
+                        <h4 class="post-title"><a :href="`/jobs/${data.slug}/${company.slug}`">{{ company.name }} <span class="text-sm">({{ company.location_id }})</span></a></h4>
+                        <div class="post-detail-wrap" v-html="company.content"></div>
                     </li>
                   </ul>
               </v-col>  
             </v-row>
-            </v-card> 
-        </v-container>
-    </v-app>
-    </Layout>
+          </div>
+            </div>
+    </section>
+</Layout>
 </template>
 <script>
   import {Link } from '@inertiajs/inertia-vue'
--- resources/js/pages/single-job-post.vue
+++ resources/js/pages/single-job-post.vue
@@ -1,25 +1,29 @@
 <template>
-  <Layout>
-    <v-app app>
-        <v-container>
-            <v-card class="mx-auto px-12 py-8" elevation="2">
-             <h3>{{data.company_name}}</h3>   
-            <h2>{{data.name}}</h2>
-            <br/>
-            Location : {{data.location}}<br/>
-            Date Posted: {{data.created_at}}<br/>
+<Layout>
+    <Head title="Create Job" />
+    <section class="relative" data-app>
+          <div class="max-w-6xl mx-auto px-4 sm:px-6">
+            <div class="pt-32 pb-12 md:pt-40 md:pb-20">
 
-            <div v-html="data.content"></div>
 
-            <div class="my-4 text-subtitle-1 ml-20">
-              <v-btn  :href="data.apply_url" target="_blank" text>
-                Apply on Company Website
-              </v-btn>
+            <div class="max-w-4xl mx-auto">
+                  <h3 class="company-title">{{data.company_name}}</h3>   
+                  <h2 class="post-title">{{data.name}}</h2>
+                  <span class="location">Location : {{data.location}}</span>
+                  <span class="post-date">Date Posted: {{data.created_at}}</span>
+
+                  <div v-html="data.content" class="post-detail-wrap"></div>
+
+                  <div class="my-4 text-subtitle-1 ml-20 text-center">
+                    <v-btn  :href="data.apply_url" target="_blank" class="bg-purple-600 hover:bg-purple-700">
+                      Apply on Company Website
+                    </v-btn>
+                  </div>
+              </div>
+             </div>
             </div>
-            </v-card> 
-        </v-container>
-    </v-app>
-  </Layout>  
+    </section>
+</Layout>
 </template>
 <script>
 import {Link } from '@inertiajs/inertia-vue'
