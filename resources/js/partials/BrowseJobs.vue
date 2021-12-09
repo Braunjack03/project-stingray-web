abc502ada49e0c6234afffae02ea8449bcdca3b0
Braunjack03 <Braunjack03@gmail.com>
2021-12-09 15:39:15 +0530

    home page Find a Technology section

--- resources/js/partials/BrowseJobs.vue
+++ resources/js/partials/BrowseJobs.vue
@@ -3,7 +3,48 @@
         <div class="max-w-6xl mx-auto px-4 sm:px-6 border-t border-gray-200 border-b pt-12 pb-20">
             <h2 class="h2 mb-4 darkColor text-center pb-8 md:pb-16 sectionTitle" data-aos="fade-up">Find a Technology or Start-up Job</h2>
             <div class="browseList--block">
-                <v-row>
+               
+                <v-row v-for="data in data" :key="data.id">
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            {{data.name}}
+                        </div>
+                    </v-col>
+                    <!--v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            Merchandiser Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            VLSI Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            Legal Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            Client Server Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            Export/Import Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            DBA Jobs
+                        </div>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3" class="browseList--block">
+                        <div class="browseList darkColor">
+                            Marketing Jobs
+                        </div>
+                    </v-col>
                     <v-col cols="6" sm="4" md="3" class="browseList--block">
                         <div class="browseList darkColor">
                             Engineering Jobs
@@ -43,47 +84,7 @@
                         <div class="browseList darkColor">
                             Marketing Jobs
                         </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Engineering Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Merchandiser Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            VLSI Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Legal Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Client Server Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Export/Import Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            DBA Jobs
-                        </div>
-                    </v-col>
-                    <v-col cols="6" sm="4" md="3" class="browseList--block">
-                        <div class="browseList darkColor">
-                            Marketing Jobs
-                        </div>
-                    </v-col>
+                    </v-col-->
                 </v-row>
             </div>
         </div>
@@ -92,6 +93,7 @@
 
 <script>
 export default {
-  name: 'Newsletter'
+  name: 'Newsletter',
+  props: ['data'],
 }
 </script>
