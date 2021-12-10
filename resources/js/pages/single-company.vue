3ff354e5625535fbba9cd2f77c2b5ee6f13f7555
Braunjack03 <Braunjack03@gmail.com>
2021-12-10 16:23:34 +0530

    header bar

--- resources/js/pages/single-company.vue
+++ resources/js/pages/single-company.vue
@@ -38,7 +38,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="#5d5dff">
                     <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
                     </svg>  Location
-                    <span class="loactionCity" 
+                    <span class="loactionCity" v-if="data.state" 
                       ><strong>{{ data.city }}, {{ data.state }}</strong>
                     </span>
                   </div>
