80338d54946bf03e72918d7a9f2c285bd4420657
Braunjack03 <Braunjack03@gmail.com>
2021-12-16 15:44:26 +0530

    Revert "article listing/detail page design integration"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
 {    
     public function __construct()
     {
-        $this->middleware('auth',['except' => ['home','jobs','companies','blog','hiring','articles','articleDetail']]);
+        $this->middleware('auth',['except' => ['home','jobs','companies','blog','hiring']]);
     }
     /**
      * Dashboard
@@ -55,14 +55,6 @@ class HomeController extends Controller
 
     public function blog(){
         return Inertia::render('blog');
-    }
-
-    public function articles(){
-        return Inertia::render('article');
-    }
-
-    public function articleDetail(){
-        return Inertia::render('single-article');
     }
 
     public function jobs(Request $request){
