e89e55f3ad5641865b08cbb176f585ae95070ba5
Braunjack03 <Braunjack03@gmail.com>
2021-12-20 14:56:09 +0530

    feedback changes company profile

--- resources/js/components/CompanyPagination.vue
+++ resources/js/components/CompanyPagination.vue
@@ -3,8 +3,8 @@
 
         <div class="flex flex-wrap text-gray-500 items-center justify-center -mb-1 custom-pagination">
             <div v-for="(link, p) in links" :key="p">
-                <div v-if="link.label === 5" class="mr-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500" v-html="link.label" />
-                <Link v-else class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-blue-700 text-white active': link.active }" :href="link.url" v-html="link.label" />
+                <div v-if="link.label === 5" class="mr-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-blue focus:border-indigo-500 focus:text-indigo-500" v-html="link.label" />
+                <Link v-else class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-blue focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-blue-700 text-white active': link.active }" :href="link.url" v-html="link.label" />
             </div>
         </div>
     </div>
