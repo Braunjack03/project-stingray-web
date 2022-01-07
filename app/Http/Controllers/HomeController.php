24e9bb356d5a2898b1c1a8118c3214301e0b6040
Braunjack03 <Braunjack03@gmail.com>
2022-01-07 15:24:34 +0530

    tags fixed in articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -53,7 +53,7 @@ class HomeController extends Controller
         $locations = Location::select('id','name')->get();
         $companytypes = CompanyType::select('id','name')->take(40)->get();
         
-        $latestarticles = Article::select('articles.slug','articles.header_image','articles.title','articles.content','articles.publish_date','users.id as author_id','users.name  as author_name')
+        $latestarticles = Article::select('articles.id','articles.slug','articles.header_image','articles.title','articles.content','articles.publish_date','users.id as author_id','users.name  as author_name')
         ->leftjoin('users','articles.author_id','users.id')
         ->where('is_published',1)
         ->with('tags')
