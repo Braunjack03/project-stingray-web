bac67194652492c0a50558ed61b1f3296d4ac34a
Braunjack03 <Braunjack03@gmail.com>
2022-01-13 10:24:51 +0530

    change region static for testing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -147,20 +147,12 @@ class Controller extends BaseController
     }
 
     public function getBucketImageUrl($uuid,$image_name,$type){
-        // if($type == 'company') {
-        //     return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/company/'.$uuid.'/'.$image_name);
-        // }else if($type == 'employer'){
-        //     return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/employer/'.$uuid.'/'.$image_name);
-        // }else{
-        //     return url('https://'.env('AWS_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/'.$uuid.'/'.$image_name);
-        // }
-
         if($type == 'company') {
-            return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.us-east-2.amazonaws.com/company/'.$uuid.'/'.$image_name);
+            return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/company/'.$uuid.'/'.$image_name);
         }else if($type == 'employer'){
-            return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.us-east-2.amazonaws.com/employer/'.$uuid.'/'.$image_name);
+            return url('https://'.env('AWS_COMPANY_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/employer/'.$uuid.'/'.$image_name);
         }else{
-            return url('https://'.env('AWS_BUCKET').'.s3.us-east-2.amazonaws.com/'.$uuid.'/'.$image_name);
+            return url('https://'.env('AWS_BUCKET').'.s3.'.env('AWS_DEFAULT_REGION').'.amazonaws.com/'.$uuid.'/'.$image_name);
         }
     }    
 }
