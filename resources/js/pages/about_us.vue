1ea93b97ccedea0eb058a2f90726002594814b21
Braunjack03 <Braunjack03@gmail.com>
2022-03-28 19:16:08 -0500

    update page titles

--- resources/js/pages/about_us.vue
+++ resources/js/pages/about_us.vue
@@ -1,34 +1,38 @@
 <template>
-        <Layout>
-            <Head title="About Us" />
-            <section data-app>
-                <div class="relative max-w-6xl px-4 mx-auto sm:px-6">
-                    <div class="pb-12 pt-36 md:pt-40 md:pb-20">
-                        <!-- Page header -->
-                        <div class="max-w-3xl pb-12 mx-auto text-center md:pb-16">
-                            <h1 class="mb-4 text-gray-700 h1" data-aos="fade-up">About Us</h1>
-    
-                        </div>
-                        <div class="contentPage">
-
-    
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </Layout>
+  <Layout>
+    <Head title="About Us" />
+    <section data-app>
+      <div class="relative max-w-6xl px-4 mx-auto sm:px-6">
+        <div class="pb-12 pt-36 md:pt-40 md:pb-20">
+          <!-- Page header -->
+          <div class="max-w-3xl pb-12 mx-auto text-center md:pb-16">
+            <h1 class="mb-4 text-gray-700 h1" data-aos="fade-up">About Us</h1>
+          </div>
+          <div class="contentPage"></div>
+        </div>
+      </div>
+    </section>
+  </Layout>
 </template>
 
 <script>
-import Layout from './Layout';
-import { Head, Link } from '@inertiajs/inertia-vue';
-import Footer from '../partials/Footer.vue';
+import Layout from "./Layout";
+import { Head, Link } from "@inertiajs/inertia-vue";
+import Footer from "../partials/Footer.vue";
 
 export default {
-    components: {
-        Head,
-        Layout,
-        Footer,
-    },
-}
+  metaInfo: {
+    title: "About Us",
+    titleTemplate: "%s - Made in Tampa",
+    meta: [
+      { name: "description", content: "Companies Hiring in the Tampa Bay" },
+    ],
+  },
+
+  components: {
+    Head,
+    Layout,
+    Footer,
+  },
+};
 </script>
