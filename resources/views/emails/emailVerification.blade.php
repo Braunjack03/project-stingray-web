f3bf881226a3c2021365e48f494960ffe6a1e42b
Braunjack03 <Braunjack03@gmail.com>
2022-04-16 18:55:33 -0400

    update emails

--- resources/views/emails/emailVerification.blade.php
+++ resources/views/emails/emailVerification.blade.php
@@ -1,4 +1,12 @@
-<h1>Thanks for Registering</h1>
+<h1>Verify Your Email Address</h1>
   
-Please verify your email with below link: 
-<a href="{{ route('user.verify', $token) }}">Verify Email</a>
+<p>As an extra security measure, please verify this is the correct email address for your Made in Tampa account.</p>
+<p>
+
+@component('mail::button', ['url' => route('user.verify', $token)])
+Confirm Email
+@endcomponent
+ 
+
+
+</p>
--- resources/views/emails/welcomeMessage.blade.php
+++ resources/views/emails/welcomeMessage.blade.php
@@ -1,5 +1,11 @@
-<html>
-<h1>Welcome to Made in Tampa</h1>
+<h1>Welcome to Made in Tampa!</h1>
 
-<p>You are successfully registered with us. Click here to <a href="{{ route('login',) }}">Login</a></p>
-</html>
+<p>Glad to have you join Tampa Bay's booming tech and start-up community. It’s my personal mission to keep you ahead of curve by letting you know everything that's happening around new start-ups in Tampa, companies moving to the area, and of course of innovative jobs.</p>
+
+<p>Feel free to contact me at jason@madeintampa.io with any questions or comments.</p>
+
+
+@component('mail::button', ['url' => route('login',)])
+        Login
+@endcomponent
+
