c84c6b9d5def917d2dd7c9be94fcec5ec2a935ae
Braunjack03 <Braunjack03@gmail.com>
2022-05-02 21:00:39 -0400

    fix pw reset email

--- app/Mail/ResetPassword.php
+++ app/Mail/ResetPassword.php
@@ -10,14 +10,15 @@ use Illuminate\Queue\SerializesModels;
 class ResetPassword extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $token;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($token)
     {
+        $this->token = $token;
         //
     }
 
