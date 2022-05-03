1f2a5e71ce2efd4e1a36798e9e7ba5d453f4fad4
Braunjack03 <Braunjack03@gmail.com>
2022-05-02 20:39:32 -0400

    fix pw reset email

--- resources/views/emails/forgetPassword.blade.php
+++ resources/views/emails/forgetPassword.blade.php
@@ -1,4 +1,10 @@
-<h1>Forget Password Email</h1>
-   
-You can reset password from bellow link:
-<a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
+@component('mail::message')
+# Forgot your password?
+  
+No problem! Click on the button below to reset your password.
+
+@component('mail::button', ['url' => url("reset-password/{$token}")])
+Reset My Password
+@endcomponent
+ 
+@endcomponent
