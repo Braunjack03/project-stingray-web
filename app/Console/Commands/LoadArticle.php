a24eddf9e31485a5c1ebb7a02b8b105e833176b4
Braunjack03 <Braunjack03@gmail.com>
2022-07-24 14:43:47 -0400

    add better error handling

--- app/Console/Commands/LoadArticle.php
+++ app/Console/Commands/LoadArticle.php
@@ -44,17 +44,24 @@ class LoadArticle extends Command
      */
     public function handle()
     {
-         $article = $this->argument('article_file');
+        $article = $this->argument('article_file');
 
         $object = YamlFrontMatter::parse(file_get_contents($article));
 
         $companies = $this->find_keywords($object->body());
         $u_companies = array_unique($companies[0]);
-        $slugs = str_replace(["{{","}}"], "", $u_companies);
+        $slugs = str_replace(["{{", "}}"], "", $u_companies);
 
         $body =  $object->body();
-        foreach($slugs as $c){
+        foreach ($slugs as $c) {
+
+            try{
             $company_profile = CompanyProfile::where('slug', $c)->first();
+            } catch(Exception $e) {
+                print("Caught exception" . $e->getMessage() . "\n");
+                dd($c);
+
+            }
             $name = $company_profile->name;
             $com = "{{{$c}}}";
             $body = str_replace($com, "<a class=\"underline\" href=\"/companies/$c\">$name</a>", $body);
@@ -65,31 +72,30 @@ class LoadArticle extends Command
         $post->title = $object->title;
         $post->sub_title = $object->sub_title;
 
-        $header_image = Storage::disk('s3Company')->putFileAs('article/images/', __DIR__."/../../../articles/images/".$object->header_image, $object->header_image);
+        $header_image = Storage::disk('s3Company')->putFileAs('article/images/', __DIR__ . "/../../../articles/images/" . $object->header_image, $object->header_image);
         $post->header_image = "https://ik.imagekit.io/stingray/article/images/" . $object->header_image;
-        $post->slug = Str::of($object->title." ".rand(0, 100))->slug('-');
+        $post->slug = Str::of($object->title . " " . rand(0, 100))->slug('-');
         $post->content = $body;
 
-        
+
         $post->author_id = $object->author_id;
         $post->is_published = 1;
         $post->created_at = now();
         $post->publish_date = now();
         $post->save();
 
-        foreach($object->tags as $tag_name){
+        foreach ($object->tags as $tag_name) {
 
-            $tag = Tag::where('name', $tag_name)->first(); 
+            $tag = Tag::where('name', $tag_name)->first();
             $post->tags()->syncWithoutDetaching($tag);
         }
 
-        foreach($slugs as $c){
+        foreach ($slugs as $c) {
             try {
-            $company_profile = CompanyProfile::where('slug', $c)->first();
-            } catch(Exception $e) {
-                print("Caught exception". $e->getMessage() . "\n");
+                $company_profile = CompanyProfile::where('slug', $c)->first();
+            } catch (Exception $e) {
+                print("Caught exception" . $e->getMessage() . "\n");
                 dd($c);
-
             }
             $post->company_profiles()->syncWithoutDetaching($company_profile);
         }
@@ -99,12 +105,12 @@ class LoadArticle extends Command
         return 0;
     }
 
-    public function find_keywords($text){
+    public function find_keywords($text)
+    {
         $pattern = '/{{[a-zA-Z0-9-_]+}}/';
-        if(preg_match_all($pattern, $text, $matches)) {
+        if (preg_match_all($pattern, $text, $matches)) {
             print_r($matches);
             return $matches;
         }
-
     }
 }
